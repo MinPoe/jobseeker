a40b4aad658acde54eb8610ccb366c097a5a167f
MinPoe <minori.poedjokerto@gmail.com>
2025-04-04 22:10:25 -0700

    APITests update

--- src/test/java/com/board/jobseeker/APITests.java
+++ src/test/java/com/board/jobseeker/APITests.java
@@ -2,7 +2,6 @@ package com.board.jobseeker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
